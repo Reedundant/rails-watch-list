9ab4c7e4fcac34ef325e3b678718acbf90ccdf0b
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-11-10 14:33:17 -0500

    I don't fully understand, but we're raking GREEN BAYBEEEEE

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+
+  # validates :title, absence: false
+  validates :title, :overview, presence: true
+  validates :title, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-\# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_10_165213) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_10_190501) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_165213) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "poster_url"
   end
 
   add_foreign_key "bookmarks", "lists"
