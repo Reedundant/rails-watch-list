5917a59d0d77da92e9079b863c58a8d985ae68f0
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-11-11 15:29:58 -0500

    Stephane told me to

--- app/controllers/boomkarks_controller.rb
+++
@@ -1,2 +0,0 @@
-class BoomkarksController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [ :index, :show, :new, :create ]
+  resources :lists, only: [ :index, :show, :new, :create ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
 
   resources :movies do
   end
-  # Defines the root path route ("/")
+
+  # resources :bookmarks, only [ :destroy ]
+  # # Defines the root path route ("/")
   # root "articles#index"
 end
